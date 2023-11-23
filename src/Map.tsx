0aa982a4134b0a855d25b15929bc1301d8601153
Grisha <grisha@skipti.com>
2023-11-23 22:23:51 +0400

    feat: Added KML to GeoJSON functionality and show that on map without image overlay

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,47 +1,31 @@
 import {FC, useCallback, useEffect, useState} from "react";
-import {MapContainer, ImageOverlay, Marker, Popup} from "react-leaflet";
+import {MapContainer, ImageOverlay, Marker, Popup, GeoJSON} from "react-leaflet";
 import {CRS, LatLngBounds} from "leaflet";
 
-import geoData from "./static.json";
+import { getGeoJSON } from "./helpers";
 
 export const Map: FC = () => {
-  const [bounds, setBounds] = useState<number[][]>([]);
+  const [geoJson, setGeoJSON] = useState<any>(null);
 
-  const loadData = useCallback(() => {
-    const bounds: number[][] = [];
+  const loadData = useCallback(async () => {
+    const geoJSON = await getGeoJSON();
 
-    for (const item of geoData.data) {
-      bounds.push([
-        item.latitude,
-        item.longitude,
-      ])
-    }
-
-    setBounds(bounds);
+    setGeoJSON(geoJSON);
   }, []);
 
   useEffect(() => {
     loadData();
-  }, [loadData]);
+  }, []);
 
   return (
     <MapContainer
-      style={{height: "500px", width: "600px"}}
+      style={{height: "500px", width: "600px", border: "1px solid gray"}}
       zoom={10}
       crs={CRS.Simple}
       center={[40.184531, 44.623633]}
     >
-      {bounds && (
-        <ImageOverlay
-          url="https://www.viatransilvanica.com/assets/images/routes/acasa/hero-section.webp"
-          bounds={new LatLngBounds(bounds)}
-        >
-          <Marker position={[40.18759286174862, 44.51475598652018]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </ImageOverlay>
+      {geoJson && (
+        <GeoJSON data={geoJson} style={{ color: "green" }} />
       )}
     </MapContainer>
   );
---
+++ src/helpers.ts
@@ -0,0 +1,20 @@
+import { kml } from "@tmcw/togeojson";
+
+export const getGeoJSON = async () => {
+  const response = await fetch("/hike.kml");
+  const data = await response.text();
+
+  const xml = parseToXML(data);
+
+  return toGeoJSON(xml);
+};
+
+export const parseToXML = (kml: string) => {
+  const domParser = new DOMParser();
+
+  return domParser.parseFromString(kml, "text/xml");
+}
+
+export const toGeoJSON = (xml: Document) => {
+  return kml(xml);
+};
